885034a67923b093592e7385f910669661a6156e
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-19 14:05:06 +0200

    added an eventlistener and a function to display all messages

--- client/script.js
+++ client/script.js
@@ -1,1 +1,24 @@
 let socket = io.connect();
+
+const input = document.getElementById('postInput');
+const target = document.getElementById('target');
+const sendToAllButton = document.getElementById('toAll');
+const sendToSelfButton = document.getElementById('toSelf');
+
+sendToAllButton.addEventListener('click' , ()=>{
+    let message = input.value;
+    input.value = "";
+    socket.emit('sendToAll', message);
+    console.log(message);
+
+});
+
+socket.on("displayMessage",(message) => {
+    let display = document.createElement("p");
+    display.innerHTML = message;
+    target.appendChild(display);
+});
+
+
+
+
