eb5b6d22020ae4b23a46ebf1980a499cb5cc2e74
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-22 09:13:57 +0200

    added a promt to store the username and also a way to display online users (still with a bug)

--- client/script.js
+++ client/script.js
@@ -5,12 +5,17 @@ const target = document.getElementById('target');
 const sendToAllButton = document.getElementById('toAll');
 const sendToSelfButton = document.getElementById('toSelf');
 
-sendToAllButton.addEventListener('click' , ()=>{
-    let message = input.value;
-    input.value = "";
-    socket.emit('sendToAll', message);
-    console.log(message);
 
+
+socket.on('message',message =>{
+    console.log(message);
+});
+
+sendToAllButton.addEventListener('click',()=>{
+    let message = username+": "+ input.value
+    input.value="";
+    socket.emit('sendToAll',message);
+    console.log(message);
 });
 
 sendToSelfButton.addEventListener('click',()=>{
@@ -20,11 +25,27 @@ sendToSelfButton.addEventListener('click',()=>{
     console.log(message);
 })
 
-socket.on("displayMessage",(message) => {
+socket.on("displayMessage",(message)=> {
     let display = document.createElement("p");
     display.innerHTML = message;
     target.appendChild(display);
 });
+
+const username = prompt('Welcome! Please enter your name:')
+socket.emit("logIn", username);
+socket.on("logUsers",(usersArray)=>{
+    console.log(usersArray)
+    for (let i = 0; i <usersArray.length ; i++) {
+        let display = document.createElement('p')
+        display.innerHTML = usersArray[i];
+        target.appendChild(display)
+    }
+});
+
+
+
+
+
 
 
 
