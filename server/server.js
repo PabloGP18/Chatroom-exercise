5c2494e1aa1b26bad0cf4a6053d046a4b97f1826
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-22 09:15:40 +0200

    Added an array and a function to store the online users at server side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+
+
 const port = 8080;
+
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
+
 const server = http.createServer(app);
 server.listen(8080, () =>{
     console.log("server running on "+port);
@@ -13,17 +17,39 @@ server.listen(8080, () =>{
 const io = require('socket.io')(server);
 
 let counter = 0;
+
+let usersArray = [];
+
 io.on('connection', (socket) => {
-    counter++;
-    console.log(counter+' someone connected');
+        counter++;
+        console.log(counter+'someone connected');
 
-    socket.on('sendToAll', (message) => {
-    io.emit("displayMessage", (message));
-    });
+        //broadcast will emit to everybody except the user that is connecting
+        socket.broadcast.emit('message','A user has joined chatter');
 
-    socket.on("sendToOwn", (message) => {
+        //To send the whole array to one specific socket, you would do something like this:
+
+
+        //when there is a disconnect
+        socket.on('disconnect',()=>{
+        io.emit('message','A user has left chatter');
+        })
+
+        socket.on('sendToAll', (message) => {
+        // io emit will message every user connected
+        io.emit("displayMessage", (message));
+        });
+
+        socket.on("sendToOwn", (message) => {
+        // socket emit will only send a message to the user him self
         socket.emit("displayMessage", message);
-    });
+        });
+
+        socket.on("logIn", (username) => {
+           usersArray.push(username);
+                io.emit("logUsers",usersArray);
+        });
+
 });
 
 
