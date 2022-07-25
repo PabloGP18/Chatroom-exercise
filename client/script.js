679a7cf3c2c6105f852ed559752027aed1523674
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-25 22:57:04 +0200

    found a way to make to make the created buttons function with the eventlistener (dynamic elements)

--- client/script.js
+++ client/script.js
@@ -1,28 +1,40 @@
+//making connection to the server side and  making it available to the client side
 let socket = io.connect();
 
-// creating elements for the chatter
+// creating function for making the chat
 function Chat(){
 
+    //created div container where the elements will be placed
     const mainDivChat = document.createElement('div');
     mainDivChat.id = 'log-chat';
     mainDivChat.setAttribute('style',
         'display: flex; justify-content: center;');
     containerDiv.appendChild(mainDivChat);
 
+    //created div input where the input will be inserted into
     const divInput= document.createElement('div');
     divInput.id = 'input-box';
     divInput.setAttribute('style',
         'display: flex; justify-content: center; margin-top: 10px;');
     containerDiv.appendChild(divInput);
 
+    //created div users where the list of users will be inserted into
+    const divUsers = document.createElement('div');
+    divUsers.id = 'users-box';
+    divUsers.setAttribute('style',
+        'height: 600px; width: 195px; opacity: 80%; background-color: white; border-right: 1px solid black;');
+    mainDivChat.appendChild(divUsers);
+
+    //function to createElement chat
     function createChatBox() {
         const chat = document.createElement('div');
         chat.id = "chat-container";
         chat.setAttribute('style',
-            'background-color:white; height:600px; width:600px; opacity:80%;');
+            'background-color:white; height:600px; width:715px; opacity:80%;');
         mainDivChat.appendChild(chat);
         }
 
+    //function to createElement button for the button to send to all the users
     function creatButtonToAll() {
         const divBtnToAll = document.createElement('div');
         divBtnToAll.id = 'div-btn-to-all';
@@ -37,6 +49,7 @@ function Chat(){
         divInput.appendChild(divBtnToAll);
     }
 
+    //function to createElement button for sending to self
     function creatButtonToSelf() {
         const divBtnToSelf = document.createElement('div');
         divBtnToSelf.id = 'div-btn-to-self';
@@ -51,72 +64,104 @@ function Chat(){
         divInput.appendChild(divBtnToSelf);
     }
 
+    //function to create an input field
     function creatInput() {
       const input = document.createElement('input');
       input.id = 'postInput';
       input.type = 'text';
-      input.setAttribute('style', 'width:28%; margin-right: 2px; background-color:whitesmoke; opacity: 80%')
+      input.setAttribute('style', 'width:50%; margin-right: 2px; background-color:whitesmoke; opacity: 80%')
       divInput.appendChild(input);
     }
 
+    //function to create a users list
+    function createOnlineUsers() {
+        const onlineUsers = document.createElement('li');
+        onlineUsers.id = 'onlineUsers';
+        onlineUsers.textContent = 'Online Users';
+        onlineUsers.setAttribute('style', '' +
+            'text-align: center; margin-top: 10px; color: green')
+        divUsers.appendChild(onlineUsers);
+    }
+
+    //calling al the nested functions below
     createChatBox();
     creatInput();
+    createOnlineUsers()
     creatButtonToSelf();
     creatButtonToAll();
 }
 
-const input = document.getElementById('postInput');
-const target = document.getElementById('target');
-const sendToAllButton = document.getElementById('toAll');
-const sendToSelfButton = document.getElementById('toSelf');
+
+// *****************the original chatter
+//const input = document.getElementById('postInput');
+//const target = document.getElementById('target');
+//const sendToAllButton = document.getElementById('toAll');
+//const sendToSelfButton = document.getElementById('toSelf');
+//*****************
+
+//const replaceForm = document.getElementById('login-box');
 const loginButton = document.getElementById("login-button");
-const replaceForm = document.getElementById('login-box');
 const containerDiv = document.getElementById('container-login');
 // button to remove dom elements from the page
 let removeLogin = document.getElementById("login-block");
 
+//const username = prompt('Welcome! Please enter your name:')
+
+let username;
+
 loginButton.addEventListener('click',()=>{
-    removeLogin.remove();
+    username = document.getElementById("userName").value;
+    socket.emit("logIn", username);
     //removeLogin.innerHTML=""
+    removeLogin.remove();
+    //removeLogin.removeAttribute('style');
     //e.preventDefault();
     Chat();
 });
 
+//Chat();
+
 socket.on('message',message =>{
     console.log(message);
 });
+document.addEventListener('click',(e)=>{
+    if(e.target.id === 'toAll'){
+        let input = document.getElementById('postInput');
+        let message = username+": "+ input.value
+        input.value="";
+        socket.emit('sendToAll',message);
+        console.log(message);
+    }
+})
 
-sendToAllButton.addEventListener('click',()=>{
-    let message = username+": "+ input.value
-    input.value="";
-    socket.emit('sendToAll',message);
-    console.log(message);
-});
 
-sendToSelfButton.addEventListener('click',()=>{
-    let message = input.value;
-    input.value ="";
-    socket.emit('sendToOwn', message);
-    console.log(message);
+document.addEventListener('click',(e)=>{
+    if(e.target.id === 'toSelf') {
+        let input = document.getElementById('postInput')
+        let message = username+": "+ input.value ;
+        input.value = "";
+        socket.emit('sendToOwn', message);
+        console.log(message);
+    }
 })
 
 socket.on("displayMessage",(message)=> {
+    let chat = document.getElementById("chat-container");
     let display = document.createElement("p");
     display.innerHTML = message;
-    target.appendChild(display);
+    chat.appendChild(display);
 });
-const username = document.getElementById("userName").value;
-//const username = prompt('Welcome! Please enter your name:')
-socket.emit("logIn", username);
+console.log(username);
+
 socket.on("logUsers",(usersArray)=>{
     console.log(usersArray)
-    usersArray="";
+    let users = document.getElementById("onlineUsers")
+    //usersArray="";
     for (let i = 0; i <usersArray.length ; i++) {
         let display = document.createElement('p')
         display.innerHTML = usersArray[i];
-        target.appendChild(display)
+        users.appendChild(display)
     }
-
 });
 
 
